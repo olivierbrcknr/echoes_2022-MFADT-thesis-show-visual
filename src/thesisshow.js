6e786cea6b92d3c00b2c4270b34396ffb375e85a
zooofzane <yu_an12@163.com>
2022-05-05 11:03:31 -0400

    Update thesisshow.js

--- src/thesisshow.js
+++ src/thesisshow.js
@@ -1,7 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
 import Stats from 'three/examples/jsm/libs/stats.module.js';
-import './perlin.js'
+// import './perlin.js'
 import {
     OrbitControls
 } from 'three/examples/jsm/controls/OrbitControls.js'
